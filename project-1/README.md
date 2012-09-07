b660c96550205d3da56ffce9f259d5ea0fafe45f
shadownx <archer.john@gmail.com>
2012-09-06 18:27:29 -0600

    Merge branch 'master' of https://github.com/shadownx/cns3060-shadownx
    Conflicts:
    	project-1/Makefile
    	project-1/README.md

--- project-1/README.md
+++ project-1/README.md
@@ -3,6 +3,8 @@ Project1 - Make
 
 John Archer
 
-So far the hardest part was getting used to GITHUB and the different interfaces used to make changes.
-
+So far the hardest part was getting used to GITHUB and the different interfaces used to make changes.  
+It took a little bit to remember to include the ( int argc, char* argv[] ) into the initial int line  
+to get the arguments being passed into the program from the command line or Makefile to be accessable  
+for output or print statements.
 
---
+++ project-1/project-1.cpp
@@ -0,0 +1,26 @@
+/*
+Name: John Archer
+Class: CS3060
+Section: 601
+Date: 9/05/2012
+
+I declare that the following source code was written solely by me.
+I understand that copying any source code, in whole or part, constitutes cheating,and that I will receive a zero on this project if I am found in violation of this policy.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char* argv[ ] )
+{
+	printf ("John Archer\n");
+	printf ("CNS 3060 Section 601\n\n");
+	int n = 0;
+	while ( n < argc )
+	{
+		// Goes through the loop until all elements in argv have been processed.
+		printf( "%s\n", argv[n] );
+		n++;
+	}
+	return 0;  
+}
